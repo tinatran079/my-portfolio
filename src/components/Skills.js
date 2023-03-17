f1f2e73faaefdeea0f131bc065748519037d5e15
Tina Tran <tinatran079@gmail.com>
2023-03-17 16:42:04 -0500

    changed skills section

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,24 +4,46 @@ class Skills extends Component {
   render() {
     if (this.props.sharedSkills && this.props.resumeBasicInfo) {
       var sectionName = this.props.resumeBasicInfo.section_name.skills;
-      var skills = this.props.sharedSkills.icons.map(function (skills, i) {
-        return (
-          <li className="list-inline-item mx-3" key={i}>
-            <span>
-              <div className="text-center skills-tile">
-                <i className={skills.class} style={{ fontSize: "220%" }}>
-                  <p
-                    className="text-center"
-                    style={{ fontSize: "30%", marginTop: "4px" }}
-                  >
-                    {skills.name}
-                  </p>
-                </i>
-              </div>
-            </span>
-          </li>
-        );
+
+      // group skills by category
+      var groupedSkills = {};
+      this.props.sharedSkills.icons.forEach(function (skill) {
+        if (skill.category in groupedSkills) {
+          groupedSkills[skill.category].push(skill);
+        } else {
+          groupedSkills[skill.category] = [skill];
+        }
       });
+
+      // generate skills list for each category
+      var skillsByCategory = [];
+      for (var category in groupedSkills) {
+        var skills = groupedSkills[category].map(function (skill, i) {
+          return (
+            <li className="list-inline-item mx-3" key={i}>
+              <span>
+                <div className="text-center skills-tile">
+                  <i className={skill.class} style={{ fontSize: "220%" }}>
+                    <p
+                      className="text-center"
+                      style={{ fontSize: "30%", marginTop: "4px" }}
+                    >
+                      {skill.name}
+                    </p>
+                  </i>
+                </div>
+              </span>
+            </li>
+          );
+        });
+
+        skillsByCategory.push(
+          <div className="col-md-12 text-center" key={category}>
+            <h2 className="text-white">{category}</h2>
+            <ul className="list-inline mx-auto skill-icon">{skills}</ul>
+          </div>
+        );
+      }
     }
 
     return (
@@ -32,9 +54,7 @@ class Skills extends Component {
               <span className="text-white">{sectionName}</span>
             </h1>
           </div>
-          <div className="col-md-12 text-center">
-            <ul className="list-inline mx-auto skill-icon">{skills}</ul>
-          </div>
+          {skillsByCategory}
         </div>
       </section>
     );
